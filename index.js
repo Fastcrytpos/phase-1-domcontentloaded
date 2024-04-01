a95aee2937905438e677cd05a2ac6cd0c9838508
Samson Mubeya <rasspartah@gmail.com>
2024-04-01 12:35:44 +0300

    did my assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    console.log(document.querySelector('#text').textContent)
+    document.querySelector('#text').textContent="This is really cool!"
+    console.log(document.querySelector('#text').textContent)
+});
+console.log(document.querySelector('#text').textContent)
